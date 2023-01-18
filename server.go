45e0494289ee3423fe5bd9be2d8b8cc3959fdd86
Hageronng <122736492+Hageronng@users.noreply.github.com>
2023-01-19 00:44:09 +0300

    Add files via upload

--- server.go
+++ server.go
@@ -17,6 +17,7 @@ func main() {
 	server := gin.Default()
 
 	var r Reg
+	var l LogIn
 
 	server.POST("/registration", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
@@ -24,13 +25,29 @@ func main() {
 			"Name":     r.Name,
 			"Password": r.Password,
 		})
+		r.Email = l.Email
+		r.Password = l.Password
 	})
 
 	server.POST("/login", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
-			"Email":    r.Email,
-			"Password": r.Password,
+			"Email":    l.Email,
+			"Password": l.Password,
 		})
+
+		if l.Email == r.Email && l.Password == r.Password {
+			server.GET("/", func(ctx *gin.Context) {
+				ctx.JSON(200, gin.H{
+					"LogIn": "Ok",
+				})
+			})
+		} else {
+			server.GET("/", func(ctx *gin.Context) {
+				ctx.JSON(200, gin.H{
+					"LogIn": "Fail",
+				})
+			})
+		}
 	})
 
 	server.Run(":0808")
